1b4afd1fbe22a422dc0d4a1f394bfdf2982d9c8b
kevinh2810 <kevinhadisutanto@gmail.com>
2020-06-02 16:53:07 +0700

    cleaning account file

--- controllers/Accounts.js
+++ controllers/Accounts.js
@@ -25,64 +25,6 @@ module.exports = class Accounts extends BaseController {
                 return callback(null, { success: true, message: `[Accounts] Upsert ${store.accountBalances.length} data successfully` })
             })
         })
-
-        // if (store.accountTransactions.length < 1) return callback(null, null)
-
-        // const senders = this.getUniqueAccounts(store.accountTransactions, store.transactionFees, store.accBalances, 'SenderAccountAddress')
-        // const recipients = this.getUniqueAccounts(
-        //   store.accountTransactions,
-        //   store.transactionFees,
-        //   store.accBalances,
-        //   'RecipientAccountAddress'
-        // )
-        // const match = ['AccountAddress']
-
-        // const insertSenders =
-        //     senders.length > 0 &&
-        //     senders.map(i => {
-        //         const aa = util.bufferStr(i.AccountAddress)
-        //         if (typeof aa == 'string' && aa !== 'null') {
-        //             return new Promise((resolve, reject) => {
-        //                 console.log('i ======>', i)
-        //                     // const params = { AccountAddress: i.AccountAddress }
-        //                 this.service.upserts(i, match, (err, result) => {
-        //                     if (err) return reject(`[Accounts] Accounts Service - Created Only New ${err}`)
-        //                     if (!result) return resolve(0)
-        //                     return resolve(1)
-        //                 })
-        //             })
-        //         }
-        //     })
-
-        // const insertRecipients =
-        //   recipients.length > 0 &&
-        //   recipients.map(i => {
-        //     const aa = util.bufferStr(i.AccountAddress)
-        //     if (typeof aa == 'string' && aa !== 'null') {
-        //       return new Promise((resolve, reject) => {
-        //         console.log('i ======>', i)
-        //         // const params = { AccountAddress: i.AccountAddress }
-        //         this.service.upserts(i, match, (err, result) => {
-        //           if (err) return reject(`[Accounts] Accounts Service - Created Only New ${err}`)
-        //           if (!result) return resolve(0)
-        //           return resolve(1)
-        //         })
-        //       })
-        //     }
-        //   })
-
-        // let accounts = insertSenders || []
-        // if (insertRecipients && insertRecipients.length > 0) {
-        //   insertRecipients.forEach(i => accounts.push(i))
-        // }
-
-        // Promise.all(accounts)
-        //   .then(results => {
-        //     const count = results.reduce((prev, curr) => parseInt(prev) + parseInt(curr), 0)
-        //     if (count < 1) return callback(null, null)
-        //     return callback(null, `[Accounts] Insert ${count} data successfully`)
-        //   })
-        //   .catch(error => callback(error, null))
     }
 
     getUniqueAccounts(arr, arr1, callback) {
