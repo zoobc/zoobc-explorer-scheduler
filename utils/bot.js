f613d8f3687939061bc29b7349441f7a5938e8f2
eksant <eksant@gmail.com>
2020-06-16 14:52:42 +0800

    Fixing conflict duplicate telegram bot

--- utils/bot.js
+++ utils/bot.js
@@ -1,24 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api')
-
-const store = require('./store')
 const config = require('../config')
 const token = config.app.tokenTelegram
-
-const getChatId = () => {
-  return store.chatId ? store.chatId : config.app.chatId
-}
 
 const teleBot = token ? new TelegramBot(token, { polling: true }) : null
 teleBot &&
   teleBot.on('message', msg => {
-    store.chatId = msg.chat.id
-    teleBot.sendMessage(getChatId(), '✨ Thank you for starting ZooBC Explorer Alert')
+    teleBot.sendMessage(msg.chat.id, '✨ Thank you for starting ZooBC Explorer Alert')
   })
 
 /** parseMode : HTML, Markdown, MarkdownV2 */
 const sendMessage = async (msg, parseMode = null) => {
   const options = parseMode ? { parse_mode: parseMode } : {}
-  const chatId = getChatId()
+  const chatId = config.app.chatId
+
   teleBot && chatId && teleBot.sendMessage(chatId, msg, options)
 }
 
