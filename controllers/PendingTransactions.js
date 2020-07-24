b58342030e936d37d9bbbcf9e09b23047b519d80
kevinh2810 <kevinhadisutanto@gmail.com>
2020-07-24 10:40:57 +0800

    resolve conflict

--- controllers/PendingTransactions.js
+++ controllers/PendingTransactions.js
@@ -22,7 +22,6 @@ module.exports = class PendingTransaction extends BaseController {
         )
       if (!res) return callback(response.setResult(false, '[Pending Transaction] No additional data'))
 
-      let count = 0
       res.forEach(txHash => {
         count++
         const params = { TransactionHashHex: Buffer.from(txHash.TransactionHash.toString('binary'), 'ascii').toString('hex') }
@@ -62,7 +61,8 @@ module.exports = class PendingTransaction extends BaseController {
             }
 
             this.transactionsService.getTestFindAndUpdate(payloads, (err, res) => {
-              if (err) return callback(response.setResult(false, `[Pending Transaction] Upsert - ${err}`))
+              // if (err) return callback(response.setResult(false, `[Pending Transaction] Upsert - ${err}`))
+              if (err) return resolve(response.sendBotMessage('Pending Transaction', `[Pending Transaction] Upsert - ${err}`))
 
               return callback(response.setResult(true, `[Pending Transaction] Upsert data successfully`))
             })
@@ -78,8 +78,6 @@ module.exports = class PendingTransaction extends BaseController {
           })
         })
       })
-
-      // return callback(response.setResult(true, `[Queue] ${count} Pending Transaction on processing`))
     })
   }
 }
--- schedulers.js
+++ schedulers.js
@@ -7,14 +7,13 @@ const { UI } = require('bull-board')
 
 const config = require('./config')
 const { msg, util, response } = require('./utils')
-const { Nodes, Blocks, Accounts, ResetData, Transactions } = require('./controllers')
+const { Nodes, Blocks, Accounts, ResetData, Transactions, PendingTransaction } = require('./controllers')
 const fetch = require('node-fetch')
 
 const nodes = new Nodes()
 const blocks = new Blocks()
 const reset = new ResetData()
 const accounts = new Accounts()
-// const rollback = new Rollback()
 const transactions = new Transactions()
 const pendingTx = new PendingTransaction()
 
