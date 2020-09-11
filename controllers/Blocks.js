33ee800bd92e50740aadfbcc9ede4054ecb6830a
gedeyandi456 <gede.yandi@blockchainzoo.com>
2020-09-11 15:59:19 +0800

    return empty array for skippedblocksmiths and publishReceipts

--- controllers/Blocks.js
+++ controllers/Blocks.js
@@ -34,14 +34,15 @@ module.exports = class Blocks extends BaseController {
 
     const promises = blocks.map(async item => {
       const TotalRewards = parseFloat(item.TotalCoinBase) + parseFloat(item.TotalFee)
+
+      let skippedsMapped = []
+      let receiptsMapped = []
+
       const skippeds = await getSkippedBlockSmiths(item.Height)
       const receipts = await getPublishedReceipts(item.Height)
 
-      const receiptsMapped =
-        receipts &&
-        receipts.PublishedReceipts &&
-        receipts.PublishedReceipts.length > 0 &&
-        receipts.PublishedReceipts.map(i => {
+      if (receipts && receipts.PublishedReceipts && receipts.PublishedReceipts.length > 0) {
+        receiptsMapped = receipts.PublishedReceipts.map(i => {
           return {
             ...i,
             IntermediateHashes: util.bufferStr(i.IntermediateHashes),
@@ -52,17 +53,16 @@ module.exports = class Blocks extends BaseController {
             },
           }
         })
+      }
 
-      const skippedsMapped =
-        skippeds &&
-        skippeds.SkippedBlocksmiths &&
-        skippeds.SkippedBlocksmiths.length > 0 &&
-        skippeds.SkippedBlocksmiths.map(i => {
+      if (skippeds && skippeds.SkippedBlocksmiths && skippeds.SkippedBlocksmiths.length > 0) {
+        skippedsMapped = skippeds.SkippedBlocksmiths.map(i => {
           return {
             ...i,
             BlocksmithPublicKey: util.getZBCAdress(i.BlocksmithPublicKey, 'ZNK'),
           }
         })
+      }
 
       return {
         BlockID: item.ID,
